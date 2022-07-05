6fca3dfa4b4ee503ee2d67b29b04d77ff4d0aee0
Stephen <stvlley.amzn@gmail.com>
2022-07-04 23:21:10 -0400

    good night

--- components/PostWidget.js
+++ components/PostWidget.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import moment from 'moment'
+import Link from 'next/link'
+import { getRecentPosts, getSimilarPosts } from '../services';
 
-export default function PostWidget() {
+export default function PostWidget({ slug, categories }) {
+
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug)
+      .then((result) => setRelatedPosts(result))
+    } else {
+      getRecentPosts()
+      .then((result) => setRelatedPosts(result))
+    }
+
+
+  }, [slug])
+
+  console.log
+
   return (
-    <div>PostWidget</div>
+    <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
+      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+        { slug ? 'Related Posts' : 'Recent Posts'}
+      </h3>
+      {relatedPosts.map((post) => (
+        <div key={post.title} className='flex items-center w-full mb-4'>
+          <div className='w-16 flex-none'>
+            <img
+            alt={post.title}
+            height='60px'
+            width='60px'
+            className='align-middle rounded-full'
+            src={post.featuredImage.url}
+            />
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
